dc81deadc330888790b6c1888d52094ad6e16d3a
davidfortytwo <85337431+davidfortytwo@users.noreply.github.com>
2024-05-23 10:43:54 +0200

    Update SpringBoot-Scan.py
    Translated to English

--- SpringBoot-Scan.py
+++ SpringBoot-Scan.py
@@ -8,18 +8,18 @@ from inc import output, console, run ,proxycheck
 import re, binascii, argparse, sys, time
 
 def get_parser():
-    parser = argparse.ArgumentParser(usage='python3 SpringBoot-Scan.py',description='SpringBoot-Scan: 针对SpringBoot的开源渗透框架',)
-    p = parser.add_argument_group('SpringBoot-Scan 的参数')
-    p.add_argument("-u", "--url", type=str, help="对单一URL进行信息泄露扫描")
-    p.add_argument("-uf", "--urlfile", type=str, help="读取目标TXT进行信息泄露扫描")
-    p.add_argument("-v", "--vul", type=str, help="对单一URL进行漏洞利用")
-    p.add_argument("-vf", "--vulfile", type=str, help="读取目标TXT进行批量漏洞扫描")
-    p.add_argument("-d", "--dump", type=str, help="扫描并下载SpringBoot敏感文件（可提取敏感信息）")
-    p.add_argument("-p", "--proxy", type=str, default='', help="使用HTTP代理")
-    p.add_argument("-z", "--zoomeye", type=str, default='', help="使用ZoomEye导出Spring框架资产")
-    p.add_argument("-f", "--fofa", type=str, default='', help="使用Fofa导出Spring框架资产")
-    p.add_argument("-y", "--hunter", type=str, default='', help="使用Hunter导出Spring框架资产")
-    p.add_argument("-t", "--newheader", type=str, help="从TXT文件中导入自定义HTTP头部")
+    parser = argparse.ArgumentParser(usage='python3 SpringBoot-Scan.py', description='SpringBoot-Scan: An open-source penetration framework for SpringBoot')
+    p = parser.add_argument_group('SpringBoot-Scan Parameters')
+    p.add_argument("-u", "--url", type=str, help="Scan a single URL for information leakage")
+    p.add_argument("-uf", "--urlfile", type=str, help="Read target TXT file for information leakage scanning")
+    p.add_argument("-v", "--vul", type=str, help="Exploit vulnerabilities in a single URL")
+    p.add_argument("-vf", "--vulfile", type=str, help="Read target TXT file for batch vulnerability scanning")
+    p.add_argument("-d", "--dump", type=str, help="Scan and download SpringBoot sensitive files (extract sensitive information)")
+    p.add_argument("-p", "--proxy", type=str, default='', help="Use HTTP proxy")
+    p.add_argument("-z", "--zoomeye", type=str, default='', help="Export Spring framework assets using ZoomEye")
+    p.add_argument("-f", "--fofa", type=str, default='', help="Export Spring framework assets using Fofa")
+    p.add_argument("-y", "--hunter", type=str, default='', help="Export Spring framework assets using Hunter")
+    p.add_argument("-t", "--newheader", type=str, help="Import custom HTTP headers from a TXT file")
     args = parser.parse_args()
     return args
 

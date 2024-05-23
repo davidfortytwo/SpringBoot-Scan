dda9797d018aab2371b2d40a2f3cf2905ee2c3b8
davidfortytwo <85337431+davidfortytwo@users.noreply.github.com>
2024-05-23 10:48:57 +0200

    Update console.py
    Translated to English

--- inc/console.py
+++ inc/console.py
@@ -4,16 +4,16 @@
  #   AabyssZG   #
 ################
 
-from inc import output,run,vul,springcheck,zoom,fofa,poc,hunter
-import sys,asyncio
+from inc import output, run, vul, springcheck, zoom, fofa, poc, hunter
+import sys, asyncio
 
-# 控制台-参数处理和程序调用
+# Console - parameter processing and program invocation
 def SpringBoot_Scan_console(args, proxies, header_new):
     if args.url:
         urlnew = springcheck.check(args.url, proxies, header_new)
         run.url(urlnew, proxies, header_new)
     if args.urlfile:
-        asyncio.run(run.file_main(args.urlfile,proxies, header_new))
+        asyncio.run(run.file_main(args.urlfile, proxies, header_new))
     if args.vul:
         urlnew = springcheck.check(args.vul, proxies, header_new)
         vul.vul(urlnew, proxies, header_new)
@@ -23,11 +23,11 @@ def SpringBoot_Scan_console(args, proxies, header_new):
         urlnew = springcheck.check(args.dump, proxies, header_new)
         run.dump(urlnew, proxies, header_new)
     if args.zoomeye:
-        zoom.ZoomDowload(args.zoomeye,proxies)
+        zoom.ZoomDownload(args.zoomeye, proxies)
     if args.fofa:
-        fofa.FofaDowload(args.fofa,proxies)
+        fofa.FofaDownload(args.fofa, proxies)
     if args.hunter:
-        hunter.HunterDowload(args.hunter,proxies)
+        hunter.HunterDownload(args.hunter, proxies)
     else:
         output.usage()
         sys.exit()
